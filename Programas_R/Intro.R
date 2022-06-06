e48caaaedf62eb3ad9a1f52be29dfd2d27f32572
Argo-Econ <a.yesid.g@gmail.com>
2022-06-06 17:42:58 -0500

    Carga de archivo R

---
+++ Programas_R/Intro.R
@@ -0,0 +1,12 @@
+
+# ---------------------------------------------------------------------#
+# Inicio de programa ----
+# ---------------------------------------------------------------------#
+
+# llamado de librerias ----
+
+#install.packages("pacman")
+library(pacman)
+p_load(xts, lubridate, lmtest,readxl,xts,sandwich, ggridges, ggplot2)
+
+
